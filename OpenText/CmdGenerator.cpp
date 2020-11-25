f3a102acb011d525aa77cdba85f2ee29043414b5
DESKTOP-K48J64J\Family HP <neu_stone@hotmail.com>
2020-11-25 15:49:32 -0500

    CCmdGenerator_Pri has been added.

--- OpenText/CmdGenerator.cpp
+++ OpenText/CmdGenerator.cpp
@@ -2,15 +2,54 @@
 #include "CmdGenerator.h"
 #include "OTCmd.h"
 #include <sstream>
+#include <algorithm>
+
+namespace CCmdGenerator_Pri
+{
+	struct CCmdInfo
+	{
+		char* pstrCmd;
+		char* pstrDesc;
+	};
+	const CCmdInfo pstrCmds[] = {
+		{"SET", "SET propertyname=newvalue"},
+		{"GET", "GET propertyname|*"},
+	};
+	enum CCmd_
+	{
+		CCmd_Set = 0,
+		CCmd_Get,
+		///.
+		CCmd_Total,
+	};
+
+	int getCmdId(const std::string& strCmd)
+	{
+		int nRet = -1;
+		std::string strTemp(strCmd);
+		std::transform(strTemp.begin(), strTemp.end(), strTemp.begin(), ::toupper);
+		for (int i=0; i<CCmd_Total; ++i)
+		{
+			if (pstrCmds[i].pstrCmd == strTemp)
+			{
+				nRet = i;
+				break;
+			}
+		}
+		return nRet;
+	}
+
+}///. end of CCmdGenerator_Pri
 
 
 int CCmdGenerator::help(std::string& strCmds)
 {
 	int nRet = 0;
 	std::stringstream ssTemp;
-	ssTemp << "SET propertyname=newvalue" << std::endl
-		<< "GET propertyname" << std::endl
-		<< "GET *" << std::endl;
+	for (int i=0; i<CCmdGenerator_Pri::CCmd_Total; ++i)
+	{
+		ssTemp << CCmdGenerator_Pri::pstrCmds[i].pstrDesc << std::endl;
+	}
 	strCmds = ssTemp.str();
 	return nRet;
 }
@@ -20,21 +59,26 @@ int CCmdGenerator::commit(
 		std::string& strResponse)
 {
 	int nError = 0;
-	if ("GET" == strCmd || "get" == strCmd)
+	int nCmdId = CCmdGenerator_Pri::getCmdId(strCmd);
+	switch (nCmdId)
 	{
-		CCmdGet cmd(strParam);
-		nError = cmd.commit(strResponse);
-	}
-	else if ("SET" == strCmd || "set" == strCmd)
-	{
-		CCmdSet cmd(strParam);
-		nError = cmd.commit(strResponse);
-	}
-	else
-	{
+	case CCmdGenerator_Pri::CCmd_Set:
+		{
+			CCmdSet cmd(strParam);
+			nError = cmd.commit(strResponse);
+		}
+		break;
+	case CCmdGenerator_Pri::CCmd_Get:
+		{
+			CCmdGet cmd(strParam);
+			nError = cmd.commit(strResponse);
+		}
+		break;
+	default:
 		///. unknown command
 		strResponse = "The command has wrong name.";
 		nError = -1;
+		break;
 	}
 	return nError;
 }
